ced040b0e3292d0e3fd404703ff6c13708156668
dshaw1242 <david.shaw1242@gmail.com>
2021-03-31 18:44:28 -0600

    add data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -215,44 +215,19 @@ var saveTasks = function(){
 }
 
 var loadTasks = function(){
-    tasks = localStorage.getItem("tasks", tasks);
-    if(tasks === null){
-        tasks = [];
+    savedTasks = localStorage.getItem("tasks", tasks);
+    if(savedTasks === null){
         return false;
     } 
-    tasks = JSON.parse(tasks);
-    for(var i = 0; i < tasks.length; i++){
+    savedTasks = JSON.parse(savedTasks);
+    for(var i = 0; i < savedTasks.length; i++){
+        // pass each task object into the createTaskEl() function
+        createTaskEl(savedTasks[i]);
+    }
         
-        tasks[i].id = taskIdCounter;
-        
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.setAttribute("data-task-id", taskIdCounter);
-        
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
-        taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-
-        taskActionsEl = createTaskActions(tasks[i].id);
-
-        listItemEl.appendChild(taskActionsEl);
-        if(tasks[i].status === "to do"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        } else if(tasks[i].status === "in progress"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        } else if(tasks[i].status === "complete"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-
-        taskIdCounter++;
-        console.log(listItemEl);
     }
     
-}
+
 loadTasks();
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
